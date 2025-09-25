e35fbccbec4654f58e14a31c2167d59742988121
Hamza Faheem <hamzafaheem216@gmail.com>
2025-09-25 15:36:05 +0500

    Change in Tab Logo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import hamzaLogoFavicon from './Assets/hamzalogotab.png';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,28 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Replace the page favicon with the navbar logo at runtime (works in dev & production)
+try {
+  const setFavicon = (href) => {
+    const head = document.getElementsByTagName('head')[0];
+    let link = head.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      head.appendChild(link);
+    }
+    link.type = 'image/png';
+    link.href = href;
+  };
+
+  // Wait until the root element is present and then set the favicon
+  if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    setFavicon(hamzaLogoFavicon);
+  } else {
+    window.addEventListener('DOMContentLoaded', () => setFavicon(hamzaLogoFavicon));
+  }
+} catch (e) {
+  // non-fatal if running in a non-browser environment
+  // console.warn('Could not set favicon at runtime', e);
+}
